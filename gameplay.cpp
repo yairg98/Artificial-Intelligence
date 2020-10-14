dab1604669ff2a49c782527a6af94e5d749c9d9c
yairg98 <yairg98@gmail.com>
2020-10-14 15:52:01 -0400

    Minor change

--- gameplay.cpp
+++ gameplay.cpp
@@ -23,11 +23,10 @@ int main() {
 	othello game(corner_test, 1);
 	
 	int move;
-    string player;
     int state = 1;
 	while(state == 1) {
 		game.print();
-		cout << (game.turn==1 ? "Black's turn" : "White's turn") << endl;
+		cout << "PLAYER " << game.turn << endl;
         cout << "Select a move: " << endl;
 		cin >> move;
 		state = game.doMove(move);
