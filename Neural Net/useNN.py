ea37d2b56228dee02a0809f63ee16e5a85220381
yairg98 <57117865+yairg98@users.noreply.github.com>
2020-11-17 16:41:52 -0500

    Minor update
    Little formatting changes

--- Neural Net/useNN.py
+++ Neural Net/useNN.py
@@ -3,13 +3,16 @@
 """
 from BPNN import *
 
+
 # Open the input file
 filename = input("Enter the name of the input file: ")
 infile = open(filename, 'r')
 lines = infile.readlines()
 
+
 # Read NN dimensions from fist line of input file
 dims = list(map(int,lines[0].split()))
+
 
 # Read and parse initial NN weights from input file
 weights = []
@@ -18,6 +21,8 @@ for i in range(1,len(dims)):
     weights.append( [ list(map(float,line.split())) for line in lines[n : n+dims[i]] ] )
     n += dims[i]
 
+
 # Initialize the neural network with the given dimensions and weights
 NN = BPNN(dims, weights)
+
 
